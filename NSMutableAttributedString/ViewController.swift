3053504775651fc48ff62e26beb2790d9e7b62c0
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-06-08 16:48:42 +0900

    取り消し線

--- NSMutableAttributedString/ViewController.swift
+++ NSMutableAttributedString/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: "取り消し線")
+
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
+
+        testTextField.attributedText = attributeString
+        
+        
     }
 
 
